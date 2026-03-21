d94c61363c92765190a3e182c4a0fbf6bd8c7b95
miguelp001 <33874625+miguelp001@users.noreply.github.com>
2026-03-21 10:57:09 -0400

    fix: remove outdated compiled JS files, use TS source

--- shared/src/index.js
+++
@@ -1,9 +0,0 @@
-export { CombatEngine, Combatant, CombatEvent, CombatResult, NightsdeepTrait, SocialClass, Tribe, SaluwanBlessing } from './combat.js';
-export { DungeonManager, BiomeType, DungeonRoom, DungeonFloor } from './dungeon.js';
-export { ItemGenerator, Rarity, Item, ItemType } from './items.js';
-export { NPCGenerator } from './party.js';
-export { GateManager, GateMilestone } from './gate.js';
-export { OfflineEngine } from './offline.js';
-export { BaseClass, SubClass, Stats, CharacterStats, StatCalculator } from './stats.js';
-export { LineageManager } from './lineage.js';
-export { EnemyGenerator } from './enemyGenerator.js';
